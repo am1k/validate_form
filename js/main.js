2c2a6b915c1d4288a49d53e5249ecdd60c0f0e06
am1k <vitaliy_bog@mail.ru>
2015-08-06 18:04:18 +0300

    change functions

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
  * Created by v.bogoroditskiy on 8/5/2015.
  */
 function ValidateForm(){
+    this.rules = {
+        required: /^[a-zA-Zà-ÿÀ-ß]+$/,
+        email: /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/,
+        telephone: /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/,
+        select: function(el){
+            if (el.options[el.selectedIndex].index == 0){
+                return true;
+            }
+        }
+    };
     this.init();
 }
 
@@ -14,54 +24,37 @@ ValidateForm.prototype = {
         var self = this;
         document.querySelector('#form').addEventListener('click', function(e) {
             if(e.srcElement.tagName.toLocaleLowerCase() === 'button') {
-                self.firstName();
-                self.lastName();
-                self.checkEmail();
-                self.checkTelephone();
-                self.checkSelect();
+                self.checkRequired();
                 self.checkForm(e);
             }
         })
     },
-    firstName: function(){
-        this.checkInput = document.querySelector('#name').value;
-        this.validName = /^[a-zA-Zà-ÿÀ-ß]+$/;
-        if(!this.validName.test(this.checkInput)){
-            document.querySelector('#name').classList.add('error');
-        }
-    },
-    lastName: function() {
-        this.checkInput = document.querySelector('#surname').value;
-        this.validName = /^[a-zA-Zà-ÿÀ-ß]+$/;
-        if(!this.validName.test(this.checkInput)){
-            document.querySelector('#surname').classList.add('error');
-        }
-    },
-    checkEmail: function() {
-        this.checkInput = document.querySelector('#email').value;
-        this.validName = /^[-._a-z0-9]+@(?:[a-z0-9][-a-z0-9]+\.)+[a-z]{2,6}$/;
-        if (!this.validName.test(this.checkInput)) {
-            document.querySelector('#email').classList.add('error');
-        }
-    },
-    checkTelephone: function() {
-        this.checkInput = document.querySelector('#telephone').value;
-        this.validName = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
-        if (!this.validName.test(this.checkInput)) {
-            document.querySelector('#telephone').classList.add('error');
-        }
-    },
-    checkSelect: function(){
-        this.checkSel = document.querySelector('#select-value').value;
-        if(this.checkSel == 0) {
-            document.querySelector('#select-value').classList.add('error');
-        }
-    },
     checkForm: function(e){
-        this.submitButton = document.querySelector('#submit');
         this.childElements = document.querySelector('#form').getElementsByClassName('error');
         if(this.childElements.length > 0 ) {
             e.preventDefault();
+        }
+    },
+    checkRequired: function(){
+        var rule;
+        this.els = document.querySelector('#form').querySelectorAll('[data-pattern]');
+        Array.prototype.forEach.call(this.els, function(el){
+            rule = this.rules[el.getAttribute('data-pattern')];
+            if(el.getAttribute('required') != null || el.value.length){
+                if(typeof rule === 'function'){
+                    this.setState(rule(el), el);
+                }else{
+                    this.setState(!rule.test(el.value), el);
+                }
+            }
+
+        }.bind(this))
+    },
+    setState: function(err, el){
+        if(err){
+            el.classList.add('error');
+        }else{
+            el.classList.remove('error');
         }
     }
 };
